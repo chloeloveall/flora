90095d1590431e300f6714054ac6648479403093
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-03 20:25:11 -0700

    Add router and routes, add login and register imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import React from 'react';
+import Register from "./Auth/Register";
+import Login from "./Auth/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './Header/Header';
 
-function App() {
-  return (
-    <>
-      
-    </>
+const App = () => {
+  return ( 
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          Placeholder
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
