2e80ba31eb8c41729a4db7427190e67e0e617a75
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-06 09:21:37 -0700

    Make app stateful, remove router, add exact to routes, add AppWithAuth to use withRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import Login from "./Auth/Login";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Switch, Route, withRouter } from "react-router-dom";
 import Header from './Main/Header/Header';
+import firebase from "../firebase";
 
-const App = () => {
-  return ( 
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <Header />
-          Home placeholder
-        </Route>
-      </Switch>
-    </Router>
-  );
+class App extends React.Component {
+  // execute listener when root component mounts
+  componentDidMount() {
+    //listener detects whether we have a user in our app
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push('/');
+      }
+    })
+  }
+
+  render () {
+    return ( 
+      // <Router>
+        <Switch>
+          <Route exact path="/login" component={Login}>
+            <Login />
+          </Route>
+          <Route exact path="/" component={App}>
+            <Header />
+            Home placeholder
+          </Route>
+        </Switch>
+      // </Router>
+    );
+  }
 }
 
-export default App;
+const AppWithAuth = withRouter(App);
+
+export default AppWithAuth;
