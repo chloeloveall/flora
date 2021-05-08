32d714db5445a9ce4ebb98f31a88dcd3858e92de
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-08 10:56:04 -0700

    Add dashboard module

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Header from "./Header";
+import PlantControl from "./PlantControl/PlantControl";
+import Header from '../Header/Header';
+
+const Dashboard = () => {
+  return ( 
+    <React.Fragment>
+    test
+      <Header />
+      <PlantControl />
+    </React.Fragment>
+  );
+}
+
+export default Dashboard;
