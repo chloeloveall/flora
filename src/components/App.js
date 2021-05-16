20bc8cca6e9da8c48e375a1d40f258818d28a954
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-15 22:37:56 -0700

    Update routes and add to clear plant create/edit form

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, useLocation } from 'react-router-dom';
 import Home from './Home/Home';
 import Nav from './NavBar/Nav';
-import PlantDashboard from './PlantDashboard';
+import PlantDashboard from './PlantDashboard/PlantDashboard';
 import PlantDetailedPage from './PlantDetailedPage/PlantDetailedPage';
-import PlantForm from './PlantForm';
+import PlantForm from '../components/PlantForm/PlantForm';
 
 const App = () => {
+  // this clears to new event form without old data
+  const {key} = useLocation();
+
   return (
     <>
       <Route exact path ='/' component={Home} />
@@ -16,7 +19,7 @@ const App = () => {
           <Nav />
           <Route exact path='/plants' component={PlantDashboard} />
           <Route path='/plants/:id' component={PlantDetailedPage} />
-          <Route path={['/createPlant', '/manage/:id']} component={PlantForm} />
+          <Route path={['/createPlant', '/manage/:id']} component={PlantForm} key={key} />
         </>
       )} />
     </>
