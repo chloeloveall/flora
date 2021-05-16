f93b6ddb85784aba95838242a7912b7f9db96775
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-15 22:32:55 -0700

    Add action types for plant CRUD

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,13 @@
+export const CREATE_PLANT = 'CREATE_PLANT';
+export const UPDATE_PLANT = 'UPDATE_PLANT';
+export const DELETE_PLANT = 'DELETE_PLANT';
+
+// export const REQUEST_FORECAST='REQUEST_FORECAST'
+// export const GET_FORECAST_FAILURE='GET_FORECAST_FAILURE'
+// export const GET_FORECAST_SUCCESS='GET_FORECAST_SUCCESS'
+
+// export const DELETE_PLANT = 'DELETE_PLANT';
+// export const TOGGLE_FORM = 'TOGGLE_FORM';
+// export const MAKE_NULL = 'MAKE_NULL';
+// export const SELECT_PLANT = 'SELECT_PLANT';
+// export const SET_EDIT = 'SET_EDIT';
