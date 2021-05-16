c9180fe3e4067dc4882f4dfc75196874995d5ed7
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-15 22:42:16 -0700

    Remove firestore, change routing, add ScrollToTop component

--- src/index.js
+++ src/index.js
@@ -1,42 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
-import AppWithAuth from './components/App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-import { createFirestoreInstance } from 'redux-firestore';
-import firebase from "./firebase";
-import rootReducer from './reducers/index';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import 'firebase/auth';
+// import './index.css';
+import App from './components/App';
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { configureStore } from './store/configureStore';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
-const store = createStore(rootReducer);
-
-const rrfProps = {
-  firebase,
-  config: {
-      userProfile: "users",
-      useFirestoreForProfile: true,
-    },
-  dispatch: store.dispatch,
-  createFirestoreInstance
-}
+const store = configureStore();
 
 ReactDOM.render(
-  <Router>
-    <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <AppWithAuth />
-      </ReactReduxFirebaseProvider>
-    </Provider>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <ScrollToTop />
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
