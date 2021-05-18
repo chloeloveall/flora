4e1b0fbe25307c333ce45d6f27b477e3cccd1d0d
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-17 20:16:36 -0700

    Add updated firebase config

---
+++ src/firebase-firestore/firebase.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
+
+export default firebase;
+
+// const provider = new firebase.auth.GoogleAuthProvider();
+// export const signInWithGoogle = () => {
+//   auth.signInWithPopup(provider);
+// };
+
+// const provider2 = new firebase.auth.GithubAuthProvider();
+// export const signInWithGithub = () => {
+//   auth.signInWithPopup(provider2);
+// }
+
+// const provider3 = new firebase.auth.TwitterAuthProvider();
+// export const signInWithTwitter = () => {
+//   auth.signInWithPopup(provider3);
+// }
