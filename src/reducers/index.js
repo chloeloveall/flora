9f37e6a93fc42dd27c05ca256db7c4e1a58a9c1d
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-18 14:47:22 -0700

    Add moda, auth, and async reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,15 @@
 import { combineReducers } from 'redux';
 import plantReducer from './plant-reducer';
+import authReducer from './auth-reducer';
+import modalReducer from './modal-reducer';
+import asyncReducer from './async-reducer';
+
 
 const rootReducer = combineReducers({
-  plant: plantReducer
+  plant: plantReducer,
+  modals: modalReducer,
+  auth: authReducer,
+  async: asyncReducer,
 })
 
 export default rootReducer;
-
-// import { firestoreReducer } from 'redux-firestore';
-// import formVisibleReducer from './form-visible-reducer';
-// import plantListReducer from './plant-list-reducer';
-// import selectedPlantReducer from './selected-plant-reducer';
-// import editingPlantReducer from './editing-plant-reducer';
-// import { combineReducers } from 'redux';
-
-// const rootReducer = combineReducers({
-//   firestore: firestoreReducer,
-//   formVisibleOnPage: formVisibleReducer,
-//   mainPlantList: plantListReducer,
-//   selectedPlant: selectedPlantReducer,
-//   editing: editingPlantReducer,
-// });
-
-// export default rootReducer;
