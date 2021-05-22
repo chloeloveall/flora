6cfc009c9cd5492c7542f447f4afe1a0a0d1a18d
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-21 21:51:12 -0700

    Add data for plant form dropdowns

---
+++ src/data/plantData.js
@@ -0,0 +1,21 @@
+export const plantLocationData = [
+    {key: 'balcony', text: 'Balcony', value: 'balcony'},
+    {key: 'bathroom', text: 'Bathroom', value: 'bathroom'},
+    {key: 'bedroom', text: 'Bedroom', value: 'bedroom'},
+    {key: 'hall', text: 'Hall', value: 'hall'},
+    {key: 'kitchen', text: 'Kitchen', value: 'kitchen'},
+    {key: 'livingRoom', text: 'Living Room', value: 'livingRoom'},
+    {key: 'office', text: 'Office', value: 'office'},
+    {key: 'patio', text: 'Patio', value: 'patio'},
+    {key: 'porch', text: 'Porch', value: 'porch'},
+    {key: 'terrace', text: 'Terrace', value: 'terrace'},
+];
+
+export const plantTypeData = [
+    {key: 'cacti', text: 'Cacti & Succulents', value: 'cacti'},
+    {key: 'foliage', text: 'Foliage Plants', value: 'foliage'},
+    {key: 'flowering', text: 'Flowering Plants', value: 'flowering'},
+    {key: 'orchids', text: 'Orchids', value: 'orchids'},
+    {key: 'vegetables', text: 'Vegetables', value: 'vegetables'},
+    {key: 'herbs', text: 'Herbs', value: 'herbs'},
+];
