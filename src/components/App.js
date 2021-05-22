17d4c5a9afce607b0e76b55947ea15ff2a0be2fe
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-21 21:49:33 -0700

    Add initialization, additional routing, loading component, toast component, modal manager component

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import { Route, useLocation } from 'react-router-dom';
-import Home from './Home/Home';
-import Nav from './NavBar/Nav';
+import LandingPage from './Home/LandingPage/LandingPage';
+import HomePage from './Home/HomePage/HomePage';
+import AboutFlora from './Home/AboutFlora/AboutFlora';
+import Blog from './Blog/Blog';
+import Nav from './NavBar/Nav/Nav';
 import PlantDashboard from './PlantDashboard/PlantDashboard';
 import PlantDetailedPage from './PlantDetailedPage/PlantDetailedPage';
 import PlantForm from '../components/PlantForm/PlantForm';
+import ModalManager from './Reusable/Modals/ModalManager';
+import { ToastContainer } from 'react-toastify';
+import ErrorComponent from './Reusable/Errors/ErrorComponent';
+import AccountPage from './Auth/AccountPage';
+import { useSelector } from 'react-redux';
+import LoadingComponent from './Reusable/Loading/LoadingComponent';
+import ProfilePage from './Profiles/ProfilePage';
 
-const App = () => {
+export default function App() {
   // this clears to new event form without old data
   const {key} = useLocation();
+  const {initialized} = useSelector((state) => state.async);
+
+  // prevents the application from loading until the initilized flag is set to true
+  if (!initialized) return <LoadingComponent content='Loading Flora...' />
 
   return (
     <>
-      <Route exact path ='/' component={Home} />
+      <ModalManager />
+      <ToastContainer
+        position='bottom-right'
+        hideProgressBar
+      />
+        <Route exact path='/' component={LandingPage} />
 
       <Route path={'/(.+)'} render={() => (
         <>
           <Nav />
+          <Route exact path='/home' component={HomePage} />
+          <Route path='/about' component={AboutFlora} />
+          <Route path='/blog' component={Blog} />
           <Route exact path='/plants' component={PlantDashboard} />
           <Route path='/plants/:id' component={PlantDetailedPage} />
           <Route path={['/createPlant', '/manage/:id']} component={PlantForm} key={key} />
+          <Route path='/profile/:id' component={ProfilePage} />
+          <Route path='/account' component={AccountPage} />
+          <Route path='/error' component={ErrorComponent} />
         </>
       )} />
     </>
   )
 }
-
-export default App;
