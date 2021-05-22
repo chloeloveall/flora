9bd8993b5757e04f2baf7257324cf4981a27e844
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-05-21 21:34:24 -0700

    Update store configuration to use verifyAuth action

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/index';
 import thunk from 'redux-thunk';
+import { verifyAuth } from '../actions/index';
 
 export function configureStore() {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+  store.dispatch(verifyAuth());
+
+  return store;
 } 
